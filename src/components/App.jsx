ef5356bf417a2bcfb5f0e621aea5b49593923299
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2022-04-10 01:05:08 +0300

    create task4
    no style, no propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendsList } from './FriendsList/FriendsList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 
 export const App = () => {
   return (
@@ -18,7 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendsList friends={friends} />
-      ,;
+      <TransactionHistory items={transactions} />
     </>
   );
 };
